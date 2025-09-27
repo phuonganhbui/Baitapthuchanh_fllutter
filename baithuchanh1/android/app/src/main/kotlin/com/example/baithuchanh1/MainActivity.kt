f6ed3816028fd5052cb2e7bbae377028ee678fbc
phuonganhbui <phuonganhh16122004@gmail.com>
2025-09-27 17:09:35 +0700

    BTTHB1

---
+++ baithuchanh1/android/app/src/main/kotlin/com/example/baithuchanh1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.baithuchanh1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ baithuchanh3/android/app/src/main/kotlin/com/example/baithuchanh3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.baithuchanh3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
